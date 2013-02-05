fa293273f8015dfb5b094f2d2739e74d5ee8cade
Michael Haitz <michael.haitz@gmx.com>
2013-02-05 23:34:31 +0100

    add wicketstuff-scala

--- src/main/java/de/agilecoders/wicket/subcut/HomePage.scala
+++ src/main/java/de/agilecoders/wicket/subcut/HomePage.scala
@@ -5,13 +5,14 @@ import org.apache.wicket.request.mapper.parameter.PageParameters
 import org.apache.wicket.markup.html.basic.Label
 import com.escalatesoft.subcut.inject.{Injectable, BindingModule}
 import de.agilecoders.wicket.subcut.ScalaApplication._
+import org.wicketstuff.scala.ScalaWicket
 
 /**
  * simple wicket page that shows a welcome message
  *
  * @author miha
  */
-class HomePage(params: PageParameters)(implicit val bindingModule: BindingModule) extends WebPage(params) with Injectable {
+class HomePage(params: PageParameters)(implicit val bindingModule: BindingModule) extends WebPage(params) with Injectable with ScalaWicket {
 
     val message = injectOptional[String]('WelcomeMessage) getOrElse("something went wrong")
     add(new Label("message", message));
