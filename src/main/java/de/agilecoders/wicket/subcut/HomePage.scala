ce6b22e8059b36a22192bcd446ad3c0c4c7f1037
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-02-06 15:47:41 +0100

    Use a package object for the global implicit. Intorduce SubcutModel to make things more Wicket-y

--- src/main/java/de/agilecoders/wicket/subcut/HomePage.scala
+++ src/main/java/de/agilecoders/wicket/subcut/HomePage.scala
@@ -3,23 +3,16 @@ package de.agilecoders.wicket.subcut
 import org.apache.wicket.markup.html.WebPage
 import org.apache.wicket.request.mapper.parameter.PageParameters
 import org.apache.wicket.markup.html.basic.Label
-import com.escalatesoft.subcut.inject.{Injectable, BindingModule}
-import de.agilecoders.wicket.subcut.ScalaApplication._
-import org.wicketstuff.scala.ScalaWicket
 
 /**
  * simple wicket page that shows a welcome message
  *
  * @author miha
  */
-class HomePage(params: PageParameters)(implicit val bindingModule: BindingModule) extends WebPage(params) with Injectable with ScalaWicket {
+class HomePage(params: PageParameters)
+  extends WebPage(params) {
 
-    val message = injectOptional[String]('WelcomeMessage) getOrElse("something went wrong")
-    add(new Label("message", message));
+  add(new Label("message", new SubcutModel[String]('WelcomeMessage, "something went wrong")))
 
-    /**
-      * Construct.
-     */
-    def this() = this(new PageParameters())
-
+  def this() = this(new PageParameters())
 }
